12ec4e31f35a1f6679cf6b9e4e9e23e5393ebe29
dokojan-manager <dokojan.info@gmail.com>
2026-03-17 07:34:25 +0900

    アイコン修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mahjong RDX | プロ雀士エンジニア 松島リキヤ ポータルサイト",
   description: "プロ雀士エンジニア「松島リキヤ」の個人ポータルサイト。麻雀をスマートにするデジタルツール群を開発しています。",
+  icons: {
+    apple: "/apple-icon.png",
+  },
 };
 
 export default function RootLayout({
